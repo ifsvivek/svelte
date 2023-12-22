bd35f1d219a842bd420f90f8795fad943943cc08
ifsvivek <vs63961@gmail.com>
2023-12-22 23:01:35 +0530

    cat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,86 +1,34 @@
 <script>
-	const layers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+	let hereKitty = false;
 
-	let y;
+	const handleMouseenter = () => (hereKitty = true);
+	const handleMouseleave = () => (hereKitty = false);
 </script>
 
-<svelte:window bind:scrollY={y} />
+<svelte:body on:mouseenter={handleMouseenter} on:mouseleave={handleMouseleave} />
 
-<a class="parallax-container" href="https://www.firewatchgame.com">
-	{#each layers as layer}
-		<img
-			style="transform: translate(0,{(-y * layer) / (layers.length - 1)}px)"
-			src="https://www.firewatchgame.com/images/parallax/parallax{layer}.png"
-			alt="parallax layer {layer}"
-		/>
-	{/each}
-</a>
-
-<div class="text">
-	<span style="opacity: {1 - Math.max(0, y / 40)}"> scroll down </span>
-
-	<div class="foreground">
-		You have scrolled {y} pixels
-	</div>
-</div>
+<!-- creative commons BY-NC http://www.pngall.com/kitten-png/download/7247 -->
+<img
+	class:curious={hereKitty}
+	alt="Kitten wants to know what's going on"
+	src="/tutorial/kitten.png"
+/>
 
 <style>
-	.parallax-container {
-		position: fixed;
-		width: 2400px;
-		height: 712px;
-		left: 50%;
-		transform: translate(-50%, 0);
-	}
-
-	.parallax-container img {
+	img {
 		position: absolute;
-		top: 0;
 		left: 0;
-		width: 100%;
-		will-change: transform;
+		bottom: -60px;
+		transform: translate(-80%, 0) rotate(-30deg);
+		transform-origin: 100% 100%;
+		transition: transform 0.4s;
 	}
 
-	.parallax-container img:last-child::after {
-		content: '';
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		background: rgb(45, 10, 13);
-	}
-
-	.text {
-		position: relative;
-		width: 100%;
-		height: 300vh;
-		color: rgb(220, 113, 43);
-		text-align: center;
-		padding: 4em 0.5em 0.5em 0.5em;
-		box-sizing: border-box;
-		pointer-events: none;
-	}
-
-	span {
-		display: block;
-		font-size: 1em;
-		text-transform: uppercase;
-		will-change: transform, opacity;
-	}
-
-	.foreground {
-		position: absolute;
-		top: 711px;
-		left: 0;
-		width: 100%;
-		height: calc(100% - 712px);
-		background-color: rgb(32, 0, 1);
-		color: white;
-		padding: 50vh 0 0 0;
+	.curious {
+		transform: translate(-15%, 0) rotate(0deg);
 	}
 
 	:global(body) {
-		margin: 0;
-		padding: 0;
-		background-color: rgb(253, 174, 51);
+		overflow: hidden;
 	}
 </style>
