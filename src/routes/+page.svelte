a80a71b635db9f42ddcad6a297bde9c51696ca6d
ifsvivek <vs63961@gmail.com>
2023-12-22 23:13:12 +0530

    last one

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,38 @@
-<script>
-	let selection = '';
+<!-- https://eugenkiss.github.io/7guis/tasks#timer -->
 
-	const handleSelectionChange = (e) => (selection = document.getSelection());
+<script>
+	import { onDestroy } from 'svelte';
+
+	let elapsed = 0;
+	let duration = 5000;
+
+	let last_time = window.performance.now();
+	let frame;
+
+	(function update() {
+		frame = requestAnimationFrame(update);
+
+		const time = window.performance.now();
+		elapsed += Math.min(time - last_time, duration - elapsed);
+
+		last_time = time;
+	})();
+
+	onDestroy(() => {
+		cancelAnimationFrame(frame);
+	});
 </script>
 
-<svelte:document on:selectionchange={handleSelectionChange} />
+<label>
+	elapsed time:
+	<progress value={elapsed / duration} />
+</label>
 
-<p>Select this text to fire events</p>
-<p>Selection: {selection}</p>
+<div>{(elapsed / 1000).toFixed(1)}s</div>
+
+<label>
+	duration:
+	<input type="range" bind:value={duration} min="1" max="20000" />
+</label>
+
+<button on:click={() => (elapsed = 0)}>reset</button>
