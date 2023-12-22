8f61b9d614d6bce4bae836d2efc2b4103f9933e7
ifsvivek <vs63961@gmail.com>
2023-12-22 23:27:20 +0530

    yep final of final push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,86 @@
 <script>
-	let c = 0;
-	let f = 32;
+	const layers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-	function setBothFromC(value) {
-		c = +value;
-		f = +(32 + (9 / 5) * c).toFixed(1);
-	}
-
-	function setBothFromF(value) {
-		f = +value;
-		c = +((5 / 9) * (f - 32)).toFixed(1);
-	}
+	let y;
 </script>
 
-<!-- https://eugenkiss.github.io/7guis/tasks/#temp -->
-<input value={c} on:input={(e) => setBothFromC(e.target.value)} type="number" /> °C =
-<input value={f} on:input={(e) => setBothFromF(e.target.value)} type="number" /> °F
+<svelte:window bind:scrollY={y} />
+
+<a class="parallax-container" href="https://ifsvivek.vercel.app/">
+	{#each layers as layer}
+		<img
+			style="transform: translate(0,{(-y * layer) / (layers.length - 1)}px)"
+			src="https://ifsvivek.github.io/host/p/parallax{layer}.png"
+			alt="parallax layer {layer}"
+		/>
+	{/each}
+</a>
+
+<div class="text">
+	<span style="opacity: {1 - Math.max(0, y / 40)}"> scroll down </span>
+
+	<div class="foreground">
+		You have scrolled {y} pixels
+	</div>
+</div>
 
 <style>
-	input {
-		width: 5em;
+	.parallax-container {
+		position: fixed;
+		width: 2400px;
+		height: 712px;
+		left: 50%;
+		transform: translate(-50%, 0);
+	}
+
+	.parallax-container img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		will-change: transform;
+	}
+
+	.parallax-container img:last-child::after {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: rgb(45, 10, 13);
+	}
+
+	.text {
+		position: relative;
+		width: 100%;
+		height: 300vh;
+		color: rgb(220, 113, 43);
+		text-align: center;
+		padding: 4em 0.5em 0.5em 0.5em;
+		box-sizing: border-box;
+		pointer-events: none;
+	}
+
+	span {
+		display: block;
+		font-size: 1em;
+		text-transform: uppercase;
+		will-change: transform, opacity;
+	}
+
+	.foreground {
+		position: absolute;
+		top: 711px;
+		left: 0;
+		width: 100%;
+		height: calc(100% - 712px);
+		background-color: rgb(32, 0, 1);
+		color: white;
+		padding: 50vh 0 0 0;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background-color: rgb(253, 174, 51);
 	}
 </style>
